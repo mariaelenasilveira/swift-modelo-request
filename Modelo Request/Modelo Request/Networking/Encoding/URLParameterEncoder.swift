7caba1900e9a466f8399bec152739900c498ceba
Mariaelena Silveira <mariaelenasilveira@info.ufrn.br>
2019-07-30 17:38:41 -0300

    atualizando, (seguindo um tutorial) ainda incompleto!

---
+++ Modelo Request/Modelo Request/Networking/Encoding/URLParameterEncoder.swift
@@ -0,0 +1,33 @@
+//
+//  URLParameterEncoder.swift
+//  Modelo Request
+//
+//  Created by Mariaelena Nascimento Silveira on 30/07/19.
+//  Copyright © 2019 Mariaelena Nascimento Silveira. All rights reserved.
+//
+
+import Foundation
+
+public struct URLParameterEncoder: ParameterEncoder {
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        guard let url = urlRequest.url else { throw NetworkError.missingURL}
+        
+        if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            urlComponents.queryItems = [URLQueryItem]()
+            
+            for(key, value) in parameters {
+                let queryItem = URLQueryItem(name: key, value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
+                
+                urlComponents.queryItems?.append(queryItem)
+            }
+            urlRequest.url = urlComponents.url
+        }
+        
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        }
+        
+    }
+    
+    
+}
